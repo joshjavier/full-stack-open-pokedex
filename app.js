5b9209733f748c07e6cfbbf98e6f63b3fb113b1f
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2024-08-25 16:49:29 +0800

    Exercise 11.12: Health check

--- app.js
+++ app.js
@@ -10,11 +10,8 @@ app.get('/version', (_req, res) => {
   res.send('3')
 })
 
-// eslint-disable-next-line no-unused-vars
 app.get('/health', (_req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw new Error('Testing failed health check...')
-  // res.send('ok')
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
